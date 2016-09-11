d4e329fa384227b6a56850761f8014127c5d216d
JAQuent <alexander.quent@rub.de>
2016-09-11 17:37:06 +0200

    Add files via upload

--- getVoiceResponse.m
+++ getVoiceResponse.m
@@ -4,12 +4,13 @@ function [RT] = getVoiceResponse(threshold, time, filename, varargin)
 %   psychological experiments making use of the Psychtoolbox 3 (PTB-3)
 %   function PsychPortAudio(). For more details on this function see: help
 %   PsychPortAudio. Once the function is called, the function records until the
-%   the specified time elapsed and finds amplitude that is higher then the chosen 
-%   threshold. The time point of this is saved and returned. The 
+%   the specified time elapsed and tries to find the voice onset. A time point is 
+%   interpreted as the voice onset if a number (default = 20) of values exceed 
+%   the treshold within a specified time window (default = 5 msec). The 
 %   function saves the sound and a figure with the threshold and where this is reached 
 %   if a filename is provided. If this is not wished, use [] instead. If
 %   you only want to save one of them, use the optional argument
-%   'savemode'.
+%   'savemode'. Note that this function was written for a 2-channel set up.
 %
 %   Mandatory arguments:
 %    threshold   -> Value between 0 and 1.
@@ -19,9 +20,18 @@ function [RT] = getVoiceResponse(threshold, time, filename, varargin)
 %                   saved. 
 %
 %   Varargin:
+%    'timewindow'   -> The length of the time window in msec, in which additional
+%                      peaks (absolute values above threshold) need to
+%                      occur so that a value above the thresold is
+%                      interpreted as the voice onset. Default is set to 5
+%                      msec. 
+%    'npeaks'       -> Number of peaks, which have to occur within the specified 
+%                      time window so that the  a value is interpreted as the 
+%                      voice onset. Default is 20. 
 %    'savemode'     -> If 1 (default), everything is saved. If 2, only the 
 %                      .wav file is saved. If 3, only the plot is saved. 
-%    'screenflip'   -> A If provide the screen flips to background after the specified time. [handle time];
+%    'screenflip'   -> A If provide the screen flips to background after the 
+%                      specified time. [handle time];
 %    'freq'         -> For more information see PsychPortAudio('Open?') in
 %                      'freq'. Default is 44100.
 %    'latencymode'  -> For more information see PsychPortAudio('Open?') in
@@ -37,39 +47,52 @@ function [RT] = getVoiceResponse(threshold, time, filename, varargin)
 %                    recordings. 
 %                    2.1 - 8. September 2016 - Adding the possibilty to
 %                    save a plot and to flip the screen. 
+%                    2.2 - 11. September 2016 - Change the signal detection
+%                    algorithm. 
+
 %% Get time and parse input arguments
 timePoint1 = GetSecs;
 
 % Default values
-freq         = 44100; % Frequency of capture
-mode         = 2;     % Capture only
-latencyMode  = 0;     % See PsychPortAudio('Open?') -> 'reqlatencyclass'
-channels     = 2;     % For stereo capture
-RT           = [];
-idx          = [];
-flip         = 0;
-saveMode     = 1; % Saves everything
+freq            = 44100; % Frequency of capture
+mode            = 2;     % Capture only
+latencyMode     = 0;     % See PsychPortAudio('Open?') -> 'reqlatencyclass'
+channels        = 2;     % For stereo capture
+RT              = [];
+idx             = [];
+flip            = 0;
+saveMode        = 1; % Saves everything
+timeWindowMsec  = 5; % msec
+nPeaks          = 20;
 
 i = 1;
 while(i<=length(varargin))
     switch lower(varargin{i});
+        case 'timewindow'
+            i              = i + 1;
+            timeWindowMsec = varargin{i};
+            i              = i + 1;
+        case 'npeaks'
+            i              = i + 1;
+            npeaks         = varargin{i};
+            i              = i + 1;
         case 'savemode'
-            i             = i + 1;
-            saveMode      = varargin{i};
-            i             = i + 1;
+            i              = i + 1;
+            saveMode       = varargin{i};
+            i              = i + 1;
         case 'screenflip'
-            i             = i + 1;
-            screenInfo    = varargin{i};
-            flip          = 1;
-            i             = i + 1;
+            i              = i + 1;
+            screenInfo     = varargin{i};
+            flip           = 1;
+            i              = i + 1;
         case 'freq'
-            i             = i + 1;
-            freq          = varargin{i};
-            i             = i + 1;
+            i              = i + 1;
+            freq           = varargin{i};
+            i              = i + 1;
         case 'latencymode'
-            i             = i + 1;
-            latencyMode   = varargin{i};
-            i             = i + 1;
+            i              = i + 1;
+            latencyMode    = varargin{i};
+            i              = i + 1;
     end
 end
 
@@ -99,21 +122,71 @@ end
 PsychPortAudio('Stop', paHandle);
 audioData = PsychPortAudio('GetAudioData', paHandle, [], [], [], 1);
 
-%% Calculate RT
-s        = PsychPortAudio('GetStatus', paHandle);
-idx1     = min(find(abs(audioData(1,:)) >= threshold));
-idx2     = min(find(abs(audioData(2,:)) >= threshold));
-idx      = min(min([idx1 idx2]));
-RT       = idx/s.SampleRate*1000;
+%% Find voiece onset and calculate RT
+s               = PsychPortAudio('GetStatus', paHandle);
+timeWindowIdx   = round(s.SampleRate/(1000/timeWindowMsec));
 
-if  length(RT) < 1
-    RT = -99;
-else
-    RT = RT + (timePoint2 - timePoint1)*1000;
+% Channel 1
+idx1         = find(abs(audioData(1,:)) >= threshold);
+if ~isempty(idx1) % Excute only if peaks were found
+    i            = 1;
+    narrowWindow = 0; % Used for correcting if the time window is larger than idx1
+    if i + timeWindowIdx > length(idx1) % If the index exceeds the idx1 vector make window smaller
+        narrowWindow = length(idx1) - (i + timeWindowIdx);
+    end
+    timeWindow = idx1(i):(idx1(i) + timeWindowIdx);
+
+    if length(timeWindow) - length(setdiff(idx1(i:(i + timeWindowIdx + narrowWindow)), timeWindow)) < nPeaks
+        while length(timeWindow) - length(setdiff(idx1(i:(i + timeWindowIdx + narrowWindow)), timeWindow)) < nPeaks
+            i          = i + 1;
+            if i > length(idx1)
+                idx1 = [];
+                break
+            end
+            timeWindow = idx1(i):(idx1(i) + timeWindowIdx);
+            if i + timeWindowIdx > length(idx1) % If the index exceeds the idx1 vector make window smaller
+                narrowWindow = length(idx1) - (i + timeWindowIdx);
+            end
+        end
+    end
+    idx1      = idx1(i);
 end
 
-if length(idx) <1
+% Channel 2
+idx2         = find(abs(audioData(2,:)) >= threshold);
+if ~isempty(idx2) % Excute only if peaks were found
+    i            = 1;
+    narrowWindow = 0; % Used for correcting if the time window is larger than idx1
+    if i + timeWindowIdx > length(idx2)% If the index exceeds the idx1 vector make window smaller
+        narrowWindow = length(idx2) - (i + timeWindowIdx);
+    end
+    timeWindow = idx2(i):(idx2(i) + timeWindowIdx);
+
+    if length(timeWindow) - length(setdiff(idx2(i:(i + timeWindowIdx + narrowWindow)), timeWindow)) < nPeaks
+        while length(timeWindow) - length(setdiff(idx2(i:(i + timeWindowIdx + narrowWindow)), timeWindow)) < nPeaks
+            i          = i + 1;
+            if i > length(idx2)
+                idx2 = [];
+                break
+            end
+            timeWindow = idx2(i):(idx2(i) + timeWindowIdx);
+            if i + timeWindowIdx > length(idx2) % If the index exceeds the idx1 vector make window smaller
+                narrowWindow = length(idx2) - (i + timeWindowIdx);
+            end
+        end
+    end
+    idx2      = idx2(i);
+end
+
+% Find lowest
+idx       = min([idx1 idx2]);
+RT        = idx/s.SampleRate*1000;
+
+if  length(RT) < 1
+    RT  = -99;
     idx = -99;
+else
+    RT = RT + (timePoint2 - timePoint1)*1000;
 end
 
 %% Close the audio device:
