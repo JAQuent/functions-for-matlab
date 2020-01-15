e2ef0ae6499847ac2a67b384edb79c4f3c39fd21
JAQuent <alexander.quent@rub.de>
2020-01-15 16:54:46 +0000

    Version 1.10
    Added the possiblity of have three anchors points (left, middle and right). To make that possible the displayed position is now moved above the line (under the question). The relative position of the question was also changed to allow multiple line questions.

--- slideScale.m
+++ slideScale.m
@@ -1,24 +1,21 @@
-function [position, RT, answer] = slideScale(screenPointer, question, rect, endPoints, varargin)
+function [position, RT, answer] = slideScale(screenPointer, question, rect, anchors, varargin)
 %SLIDESCALE This funtion draws a slide scale on a PSYCHTOOLOX 3 screen and returns the
 % position of the slider spaced between -100 and 100 as well as the rection time and if an answer was given.
 %
-%   Usage: [position, secs] = slideScale(ScreenPointer, question, center, rect, endPoints, varargin)
+%   Usage: [position, secs] = slideScale(ScreenPointer, question, center, rect, anchors, varargin)
 %   Mandatory input:
 %    ScreenPointer  -> Pointer to the window.
 %    question       -> Text string containing the question.
 %    rect           -> Double contatining the screen size.
 %                      Obtained with [myScreen, rect] = Screen('OpenWindow', 0);
-%    endPoints      -> Cell containg the two text string of the left and right
-%                      end of the scala. Exampe: endPoints = {'left, 'right'};
+%    anchors        -> Cell containg the two or three text strings of the left (, middle) and right
+%                      end of the scala. Exampe: anchors = {'left', 'middle', 'right'}
 %
 %   Varargin:
 %    'linelength'     -> An integer specifying the lengths of the ticks in
 %                        pixels. The default is 10.
 %    'width'          -> An integer specifying the width of the scala line in
 %                        pixels. The default is 3.
-%    'range'          -> An integer specifying the type of range. If 1,
-%                        then the range is from -100 to 100. If 2, then the
-%                        range is from 0 to 100. Default is 1. 
 %    'startposition'  -> Choose 'right', 'left' or 'center' start position.
 %                        Default is center.
 %    'scalalength'    -> Double value between 0 and 1 for the length of the
@@ -27,21 +24,18 @@ function [position, RT, answer] = slideScale(screenPointer, question, rect, endP
 %                        scale. 0 is top and 1 is bottom. Default is 0.8.
 %    'device'         -> A string specifying the response device. Either 'mouse' 
 %                        or 'keyboard'. The default is 'mouse'.
-%    'responsekeys'   -> Vector containing keyCodes of the keys from the keyboard to log the
-%                        response and move the slider to the right and left. 
-%                        The default is [KbName('return') KbName('left') KbName('right')].
-%    'stepSize'       -> An integer specifying the number of pixel the
-%                        slider moves with each step. Default is 1.
-%    'slidercolor'    -> Vector for the color value of the slider [r g b] 
-%                        from 0 to 255. The default is red [255 0 0].
+%    'responsekey'    -> String containing name of the key from the keyboard to log the
+%                        response. The default is 'return'.
+%    'slidecolor'     -> Vector for the color value of the slider [r g b] 
+%                        from 0 to 255. The dedult is red [255 0 0].
 %    'scalacolor'     -> Vector for the color value of the scale [r g b] 
-%                        from 0 to 255.The default is black [0 0 0].
+%                        from 0 to 255.The dedult is black [0 0 0].
 %    'aborttime'      -> Double specifying the time in seconds after which
 %                        the function should be aborted. In this case no
-%                        answer is saved. The default is Inf.
+%                        answer is saved. The default is 8 secs.
 %    'image'          -> An image saved in a uint8 matrix. Use
 %                        imread('image.png') to load an image file.
-%    'displaypoition' -> If true, the position of the slider is displayed. 
+%    'displayposition' -> If true, the position of the slider is displayed. 
 %                        The default is false. 
 %
 %   Output:
@@ -53,34 +47,33 @@ function [position, RT, answer] = slideScale(screenPointer, question, rect, endP
 %                       variable is 1.
 %
 %   Author: Joern Alexander Quent
-%   e-mail: Alex.Quent@mrc-cbu.cam.ac.uk
+%   e-mail: alexander.quent@rub.de
 %   Version history:
-%                    1.0 - 4. January 2016 - First draft
+%                    1.0 - 4th January 2016 - First draft
 %                    1.1 - 18. Feburary 2016 - Added abort time and option to
 %                    choose between mouse and key board
-%                    1.2 - 5. October 2016 - End points will be aligned to end
+%                    1.2 - 5th October 2016 - End points will be aligned to end
 %                    ticks
-%                    1.3 - 06/01/2017 - Added the possibility to display an
+%                    1.3 - 6th January 2017 - Added the possibility to display an
 %                    image
-%                    1.4 - 5. May 2017 - Added the possibility to choose a
+%                    1.4 - 5th May 2017 - Added the possibility to choose a
 %                    start position
-%                    1.5 - 7. November 2017 - Added the possibility to display
+%                    1.5 - 7th November 2017 - Added the possibility to display
 %                    the position of the slider under the scale.
-%                    1.6 - 27. November 2017 - The function now waits until
+%                    1.6 - 27th November 2017 - The function now waits until
 %                    all keys are released before exiting. 
-%                    1.7 - 28. November 2017 - More than one screen
+%                    1.7 - 28th November 2017 - More than one screen
 %                    supported now.
-%                    1.8 - 29. November 2017 - Fixed issue that mouse is
+%                    1.8 - 29th November 2017 - Fixed issue that mouse is
 %                    not properly in windowed mode.
-%                    1.9 - 7. December 2017 - If an image is drawn, the
+%                    1.9 - 7th December 2017 - If an image is drawn, the
 %                    corresponding texture is deleted at the end.
-%                    1.10 - 28. December 2017 - Added the possibility to
-%                    choose the type of range (0 to 100 or -100 to 100).
-%                    1.11 - 7. May 2019 - Added the possibility to control
-%                    the slider with keys only. Use keyboard as devices and
-%                    select this keys for this function. In addition,
-%                    default for aborttime was changed to Inf and one bug
-%                    with slidercolor was fixed. 
+%                    1.10 - 13rd January 2020 - Added the possiblity of
+%                    have three anchors points (left, middle and right). To
+%                    make that possible the displayed position is now moved
+%                    above the line (under the question). The relative
+%                    position of the question was  also changed to allow 
+%                    multiple line questions. 
 %% Parse input arguments
 % Default values
 center        = round([rect(3) rect(4)]/2);
@@ -91,14 +84,12 @@ scalaPosition = 0.8;
 sliderColor   = [255 0 0];
 scaleColor    = [0 0 0];
 device        = 'mouse';
-aborttime     = Inf;
-responseKeys  = [KbName('return') KbName('left') KbName('right')];
+aborttime     = 8;
+responseKey   = KbName('return');
 GetMouseIndices;
 drawImage     = 0;
 startPosition = 'center';
 displayPos    = false;
-rangeType     = 1;
-stepSize      = 1;
 
 i = 1;
 while(i<=length(varargin))
@@ -110,10 +101,6 @@ while(i<=length(varargin))
         case 'width'
             i             = i + 1;
             width         = varargin{i};
-            i             = i + 1;
-        case 'range'
-            i             = i + 1;
-            rangeType     = varargin{i};
             i             = i + 1;
         case 'startposition'
             i             = i + 1;
@@ -129,19 +116,15 @@ while(i<=length(varargin))
             i             = i + 1;
         case 'device' 
             i             = i + 1;
-            device        = varargin{i};
+            device = varargin{i};
             i             = i + 1;
-        case 'responsekeys'
+        case 'responsekey'
             i             = i + 1;
-            responseKeys  = varargin{i};
+            responseKey   = KbName(varargin{i});
             i             = i + 1;
-        case 'stepsize'
+        case 'slidecolor'
             i             = i + 1;
-            stepSize      = varargin{i};
-            i             = i + 1;
-        case 'slidercolor'
-            i             = i + 1;
-            sliderColor   = varargin{i};
+            sliderColor    = varargin{i};
             i             = i + 1;
         case 'scalacolor'
             i             = i + 1;
@@ -167,7 +150,7 @@ end
 
 % Sets the default key depending on choosen device
 if strcmp(device, 'mouse')
-    mouseButton   = 1; % X mouse button
+    responseKey   = 1; % X mouse button
 end
 
 %% Checking number of screens and parsing size of the global screen
@@ -194,7 +177,12 @@ midTick    = [center(1) rect(4)*scalaPosition - lineLength - 5 center(1) rect(4)
 leftTick   = [rect(3)*(1-scalaLength) rect(4)*scalaPosition - lineLength rect(3)*(1-scalaLength) rect(4)*scalaPosition  + lineLength];
 rightTick  = [rect(3)*scalaLength rect(4)*scalaPosition - lineLength rect(3)*scalaLength rect(4)*scalaPosition  + lineLength];
 horzLine   = [rect(3)*scalaLength rect(4)*scalaPosition rect(3)*(1-scalaLength) rect(4)*scalaPosition];
-textBounds = [Screen('TextBounds', screenPointer, endPoints{1}); Screen('TextBounds', screenPointer, endPoints{2})];
+if length(anchors) == 2
+    textBounds = [Screen('TextBounds', screenPointer, sprintf(anchors{1})); Screen('TextBounds', screenPointer, sprintf(anchors{2}))];
+else
+    textBounds = [Screen('TextBounds', screenPointer, sprintf(anchors{1})); Screen('TextBounds', screenPointer, sprintf(anchors{3}))];
+end
+
 if drawImage == 1
     rectImage  = [center(1) - imageSize(2)/2 rect(4)*(scalaPosition - 0.2) - imageSize(1) center(1) + imageSize(2)/2 rect(4)*(scalaPosition - 0.2)];
     if rect(4)*(scalaPosition - 0.2) - imageSize(1) < 0
@@ -211,21 +199,7 @@ scaleRangeShifted = round((scaleRange)-mean(scaleRange));                      %
 t0                         = GetSecs;
 answer                     = 0;
 while answer == 0
-    % Parse user input for x location
-    if strcmp(device, 'mouse')
-        [x,~,buttons,~,~,~] = GetMouse(screenPointer, 1);
-    elseif strcmp(device, 'keyboard')
-        [~, ~, keyCode] = KbCheck;
-        if keyCode(responseKeys(2)) == 1
-            x = x - stepSize; % Goes stepSize pixel to the left
-        elseif keyCode(responseKeys(3)) == 1
-            x = x + stepSize; % Goes stepSize pixel to the right
-        end
-    else
-        error('Unknown device');
-    end
-    
-    % Stop at upper and lower bound
+    [x,y,buttons,focus,valuators,valinfo] = GetMouse(screenPointer, 1);
     if x > rect(3)*scalaLength
         x = rect(3)*scalaLength;
     elseif x < rect(3)*(1-scalaLength)
@@ -238,12 +212,21 @@ while answer == 0
     end
     
     % Drawing the question as text
-    DrawFormattedText(screenPointer, question, 'center', rect(4)*(scalaPosition - 0.1)); 
+    DrawFormattedText(screenPointer, question, 'center', rect(4)*(scalaPosition - 0.16)); 
     
     % Drawing the end points of the scala as text
-    DrawFormattedText(screenPointer, endPoints{1}, leftTick(1, 1) - textBounds(1, 3)/2,  rect(4)*scalaPosition+40, [],[],[],[],[],[],[]); % Left point
-    DrawFormattedText(screenPointer, endPoints{2}, rightTick(1, 1) - textBounds(2, 3)/2,  rect(4)*scalaPosition+40, [],[],[],[],[],[],[]); % Right point
-    
+    if length(anchors) == 2
+        % Only left and right anchors
+        DrawFormattedText(screenPointer, anchors{1}, leftTick(1, 1) - textBounds(1, 3)/2,  rect(4)*scalaPosition+40, [],[],[],[],[],[],[]); % Left point
+        DrawFormattedText(screenPointer, anchors{2}, rightTick(1, 1) - textBounds(2, 3)/2,  rect(4)*scalaPosition+40, [],[],[],[],[],[],[]); % Right point
+    else 
+        % Left, middle and right anchors
+        DrawFormattedText(screenPointer, anchors{1}, leftTick(1, 1) - textBounds(1, 3)/2,  rect(4)*scalaPosition+40, [],[],[],[],[],[],[]); % Left point
+        DrawFormattedText(screenPointer, anchors{2}, 'center',  rect(4)*scalaPosition+40, [],[],[],[],[],[],[]); % Middle point
+        DrawFormattedText(screenPointer, anchors{3}, rightTick(1, 1) - textBounds(2, 3)/2,  rect(4)*scalaPosition+40, [],[],[],[],[],[],[]); % Right point
+   
+    end
+
     % Drawing the scala
     Screen('DrawLine', screenPointer, scaleColor, midTick(1), midTick(2), midTick(3), midTick(4), width);         % Mid tick
     Screen('DrawLine', screenPointer, scaleColor, leftTick(1), leftTick(2), leftTick(3), leftTick(4), width);     % Left tick
@@ -254,34 +237,30 @@ while answer == 0
     Screen('DrawLine', screenPointer, sliderColor, x, rect(4)*scalaPosition - lineLength, x, rect(4)*scalaPosition  + lineLength, width);
     
     % Caculates position
-    if rangeType == 1
-        position = round((x)-mean(scaleRange));           % Calculates the deviation from the center
-        position = (position/max(scaleRangeShifted))*100; % Converts the value to percentage
-    elseif rangeType == 2
-        position = round((x)-min(scaleRange));                       % Calculates the deviation from 0. 
-        position = (position/(max(scaleRange)-min(scaleRange)))*100; % Converts the value to percentage
-    end
-
+    position          = round((x)-mean(scaleRange));           % Shift the x value according to the new scale
+    position          = (position/max(scaleRangeShifted))*100; % Converts the value to percentage
     
     % Display position
     if displayPos
-        DrawFormattedText(screenPointer, num2str(round(position)), 'center', rect(4)*(scalaPosition + 0.05)); 
+        DrawFormattedText(screenPointer, num2str(round(position)), 'center', rect(4)*(scalaPosition - 0.05)); 
     end
     
     % Flip screen
-    Screen('Flip', screenPointer);
+    onsetStimulus = Screen('Flip', screenPointer);
     
     % Check if answer has been given
     if strcmp(device, 'mouse')
         secs = GetSecs;
-        if buttons(mouseButton) == 1
+        if buttons(responseKey) == 1
             answer = 1;
         end
     elseif strcmp(device, 'keyboard')
-        [~, secs, keyCode] = KbCheck;
-        if keyCode(responseKeys(1)) == 1
+        [keyIsDown, secs, keyCode] = KbCheck;
+        if keyCode(responseKey) == 1
             answer = 1;
         end
+    else
+        error('Unknown device');
     end
     
     % Abort if answer takes too long
@@ -296,6 +275,5 @@ if drawImage == 1
     Screen('Close', stimuli);
 end
 %% Calculating the rection time and the position
-RT                = (secs - t0)*1000;                                          % converting RT to millisecond
+RT                = (secs - t0)*1000;   % converting RT to millisecond
 end
-
